8b14e8dc06c8cf477314319bc354409fde470581
hebibov666 <asuslap503@gmail.com>
2024-04-22 15:36:31 +0300

    Commit for design check

--- pages/components/CustomSelect.js
+++ pages/components/CustomSelect.js
@@ -5,7 +5,7 @@ function CustomSelect({title,options,onClick,selectOption,field,element}){
         className="relative transition-all duration-500 w-full h-[40px] overflow-hidden">
        <div className="relative h-[40px]  bg-[#1F1F1F] flex justify-between rounded border-[2px] border-[#252525] p-[5px]">
           {element==="input" ? <input type="text" placeholder={title} className='outline-none h-full text-[#9CA3AF] bg-[#1F1F1F]'></input> : <h1 className='text-[#9CA3AF]'>{title}</h1>}
-           <ArrowDropDownIcon className="absolute h-full w-[30px] right-0 top-0 text-[#9CA3AF]" />
+           <ArrowDropDownIcon style={{rotate:"-90deg"}} className="absolute h-full w-[30px] right-0 top-0 text-[#9CA3AF]" />
        </div>
        <ul className=" w-full text-[#9CA3AF] bg-[#1F1F1F] transition-all duration-500 rounded mt-2 h-[250px] scroll overflow-y-scroll">
            {options?.map((car, index) => (
